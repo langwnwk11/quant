012e8f61c18f26a917acd30888c513e352bbd49a
langwnwk11 <166970502+langwnwk11@users.noreply.github.com>
2026-03-02 11:52:03 +0800

    Update fetch_data.py

--- scripts/fetch_data.py
+++ scripts/fetch_data.py
@@ -8,7 +8,7 @@ from tenacity import retry, stop_after_attempt, wait_fixed
 # 这里的 group_index 和 total_groups 由 GitHub Action 传入
 GROUP_INDEX = int(sys.argv[1])
 #GROUP_INDEX=0
-TOTAL_GROUPS = 10
+TOTAL_GROUPS = 20
 
 @retry(stop=stop_after_attempt(3), wait=wait_fixed(2))
 def get_company_data(symbol):
